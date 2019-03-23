df6acf467298f3a15beb75a146b34dc03a752134
ziweiyee1998 <48558386+ziweiyee1998@users.noreply.github.com>
2019-03-23 23:13:40 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 # include <iostream>
 # include <string>
 # include <fstream>
+# include <cstdlib>
+
+# include "Student.h"
+# include "Information.h"
+
 using namespace std;
 
 int main()
 {
+	Student x;
+	
+	x.readDetail();
+	
+	Information y;
+	
+	y.readInfo();
+	y.printInfo();
 	
 	return 0;
 }
