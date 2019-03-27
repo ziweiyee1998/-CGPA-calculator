6e3b2c6f1ab0490dc20578e97ca5deba8a30a1e0
BubbleTeaIsLove <48561752+BubbleTeaIsLove@users.noreply.github.com>
2019-03-27 11:57:24 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,17 +8,33 @@
 
 using namespace std;
 
+void display(string name, string matric)
+{
+	ofstream outFile ("CGPA.txt");
+	
+	outFile	<<"Student's name	: "<<name<<endl
+		<<"Matric Number	: "<<matric<<endl;
+			
+	outFile.close();
+}
+
 int main()
 {
 	Student x;
 	
 	x.readDetail();
+	string nama = x.getName();
+	string matrice = x.getMatric();
 	
 	Information y;
 	
 	y.readInfo();
+	string coursec = y.getCourseCode();
+	y.extractCredit(coursec);
+	y.stringToInt();
 	y.printInfo();
+	
+	display(nama, matrice); 
 	
 	return 0;
 }
-
