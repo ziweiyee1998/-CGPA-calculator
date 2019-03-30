9ff7f91a9c3df3aa80dc5180c7b0bbccf29b1c71
ziweiyee1998 <48558386+ziweiyee1998@users.noreply.github.com>
2019-03-30 20:10:16 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 # include <string>
 # include <fstream>
 # include <cstdlib>
+# include <iosmanip>
 
 # include "Student.h"
 # include "Information.h"
@@ -26,11 +27,23 @@ int main()
 	string nama = x.getName();
 	string matrice = x.getMatric();
 	
+	int choice;
+	
+	cout<<"-----------------------------------------------------------"<<endl;
+	cout<< right << setw(38) << "GPA & CGPA Calculator" <<endl;
+	cout<<"-----------------------------------------------------------"<<endl;
+	cout<< setw(10) <<" MENU: "<<endl;
+	cout<< "1. Calculate GPA (Grade Point Average)" <<endl
+	    << "2. Calculate CGPA (Cumulative Grade Point Average)" <<endl;
+	cout<<endl;
+	cout<<"Enter your choice: ";
+	cin>>choice; 
+	
 	Information y;
 	
 	y.readInfo();
-	string coursec = y.getCourseCode();
-	y.extractCredit(coursec);
+	string subjectc = y.getSubjectCode();
+	y.extractCredit(subjectc);
 	y.stringToInt();
 	y.printInfo();
 	
